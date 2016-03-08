2cbbc17acafbe94b7cf6fe59c8bf13713b3895c7
enimstad <emma@nimstad.net>
2016-03-08 14:34:31 +0000

    Adds functionality to connect to Event Hub

---
+++ EventHubConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+class EventHubConnector
+{
+    static string eventHubName = "mvkdemoeventhub";
+    static string connectionString = "Endpoint=sb://mvkdemoeventhub-ns.servicebus.windows.net/;SharedAccessKeyName=SendRule;SharedAccessKey=1Cavv5ysfeKU9aYjESuXBm7+UKX6+qOJRG0Bioi0Rug=";
+
+    static public void SendingRandomMessages()
+    {
+        var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+        while (true)
+        {
+            try
+            {
+                var message = Guid.NewGuid().ToString();
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                Console.ResetColor();
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}

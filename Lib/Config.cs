2460e9e78ff943fb02d625ffcb6d7214051d7793
Mauritz <mauritzz@kth.se>
2016-04-01 11:52:11 +0200

    Improved and documented Config.cs

--- Lib/Config.cs
+++ Lib/Config.cs
@@ -2,24 +2,35 @@
 
 namespace AzureWSBridge.Lib
 {
+    /// <summary>
+    /// Reads configuration properties from project .config files.
+    /// </summary>
     internal static class Config
     {
-        private static readonly string _sectionName = "appSettings";
+        private const string SectionName = "appSettings";
         private static KeyValueConfigurationCollection _settings;
 
         private static void ReadSettings()
         {
+            // Don't read all settings again if they are already loaded
             if (_settings != null)
                 return;
 
-            var section = (AppSettingsSection) ConfigurationManager.GetSection(_sectionName);
+            // Fetch the KeyValueConfigurationCollection with all the settings in SectionName
+            var section = (AppSettingsSection) ConfigurationManager.GetSection(SectionName);
             _settings = section.Settings;
         }
 
+        /// <summary>
+        /// Read a given setting from the .config file.
+        /// </summary>
+        /// 
+        /// <param name="key">Key to read.</param>
+        /// <returns>Null if the given key does not exist, its value otherwise.</returns>
         public static string ReadSetting(string key)
         {
             ReadSettings();
-            return _settings[key].Value;
+            return (_settings[key] == null) ? null : _settings[key].Value;
         }
     }
 }
